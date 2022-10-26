2be121d4fa28593053632a9f4d45ad5dd999490d
Amit7366 <40818508+Amit7366@users.noreply.github.com>
2022-10-27 01:27:58 +0600

    firebase install

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBIu962QvH4IsFrqFH8mmiPb1_SKO3AZ7Y",
+  authDomain: "genius-air-school.firebaseapp.com",
+  projectId: "genius-air-school",
+  storageBucket: "genius-air-school.appspot.com",
+  messagingSenderId: "730200322292",
+  appId: "1:730200322292:web:f630339937bc5797b12ea1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
