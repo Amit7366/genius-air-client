120f6f6c6de4c64cb64d6aee5122a85ceb663ac6
Amit7366 <40818508+Amit7366@users.noreply.github.com>
2022-10-27 23:12:16 +0600

    download pdf feature

--- src/pages/Shared/Details/Details.js
+++ src/pages/Shared/Details/Details.js
@@ -1,16 +1,25 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import { FaBookmark } from "react-icons/fa";
 import { Link, useLoaderData } from "react-router-dom";
 import './Details.css';
+import Pdf from "react-to-pdf";
 
 const Details = () => {
   const topicDetails = useLoaderData();
   const {id,title,image,details,rating,price,ratings,bullet} = topicDetails;
+  const ref = React.createRef();
   return (
     <Card >
-      <Card.Header>Featured</Card.Header>
-      <Card.Body>
+      
+      <Card.Header className="text-end">
+      <Pdf targetRef={ref} filename="code-example.pdf">
+        {({ toPdf }) => <button onClick={toPdf} className="btn btn-sm btn-info"><FaBookmark></FaBookmark> </button>}
+      </Pdf>
+        
+      </Card.Header>
+      <Card.Body ref={ref}>
       <Card.Img variant="top" src={image} className='banner-image'/>
         <Card.Title>{title}</Card.Title>
         <Card.Text>
